c5bcfa50015ff27d381ec4c81fd75fb68bb440fa
bladintor297 <85669363+bladintor297@users.noreply.github.com>
2022-04-29 04:58:10 +0800

    upload

---
+++ app2.js
@@ -0,0 +1,41 @@
+(function(){
+  
+  let screen = document.querySelector('.screen');
+  let buttons = document.querySelectorAll('.btn');
+  let clear = document.querySelector('.btn-clear');
+  let equal = document.querySelector('.btn-equal');
+  let eqn = ""; // declare eqn to store equation as string
+  
+  buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+      let value = e.target.dataset.num;
+      if(value === "+" || value === "-" || value === "*" || value === "/"){
+        screen.value = '';
+        screen.value = value;
+      }else{
+        screen.value += value;
+      }
+      
+      eqn+=value; // concatenate value to become a single string of an equation
+    })
+  });
+  
+  equal.addEventListener('click', function(e){
+    if(screen.value === ''){
+      screen.value = 'Please Enter a Value';
+    } else {
+      let answer = eval(eqn); // evaluate the equation
+      screen.value = ''; // clear the displayed equation
+      eqn=''; // clear current stored equation
+      screen.value += answer; //display answer
+      eqn += screen.value; //assign current displayed value to equation
+    }
+  })
+  
+  clear.addEventListener('click', function(e){
+    screen.value = '';
+    // screen.value = 0;
+
+  })
+  
+})();
